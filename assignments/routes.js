f99eb9a2788ad00816a48a0a5d035cb43f2e02eb
carolinelilyyu <carolinelilyyu@gmail.com>
2023-11-20 16:55:54 -0500

    got rid of express

--- assignments/routes.js
+++ assignments/routes.js
@@ -1,45 +1,45 @@
-import db from "../Database/index.js";
+// import db from "../Database/index.js";
 
-function AssignmentsRoutes(app) {
-    app.delete("/api/assignments/:mid", (req, res) => {
-        const { mid } = req.params;
-        db.modules = db.modules.filter((m) => m._id !== mid);
-        res.sendStatus(200);
-      });
+// function AssignmentsRoutes(app) {
+//     app.delete("/api/assignments/:mid", (req, res) => {
+//         const { mid } = req.params;
+//         db.modules = db.modules.filter((m) => m._id !== mid);
+//         res.sendStatus(200);
+//       });
     
-  app.get("/api/courses/:cid/assignments", (req, res) => {
-    const { cid } = req.params;
-    console.log("CID IS " + cid);
-    // const modules = db.modules
-    //   .filter((m) => console.log(m.course));//m.course === cid);
-    const filteredModules = db.assignments.filter(assignment => assignment._id === cid);
-    console.log(filteredModules);
-    res.send(filteredModules);
-  });
+//   app.get("/api/courses/:cid/assignments", (req, res) => {
+//     const { cid } = req.params;
+//     console.log("CID IS " + cid);
+//     // const modules = db.modules
+//     //   .filter((m) => console.log(m.course));//m.course === cid);
+//     const filteredModules = db.assignments.filter(assignment => assignment._id === cid);
+//     console.log(filteredModules);
+//     res.send(filteredModules);
+//   });
 
-  app.post("/api/courses/:cid/assignments", (req, res) => {
-    const { cid } = req.params;
-    const newModule = {
-      ...req.body,
-      course: cid,
-      _id: new Date().getTime().toString(),
-    };
-    db.modules.push(newModule);
-    res.send(newModule);
-  });
+//   app.post("/api/courses/:cid/assignments", (req, res) => {
+//     const { cid } = req.params;
+//     const newModule = {
+//       ...req.body,
+//       course: cid,
+//       _id: new Date().getTime().toString(),
+//     };
+//     db.modules.push(newModule);
+//     res.send(newModule);
+//   });
 
 
-app.put("/api/assignments/:mid", (req, res) => {
-    const { aid } = req.params;
-    const assignmentIndex = db.assignments.findIndex(
-    (a) => a._id === aid);
-    db.assignments[assignmentIndex] = {
-    ...db.modules[assignmentIndex],
-    ...req.body
-    };
-    res.sendStatus(204);
-});
+// app.put("/api/assignments/:mid", (req, res) => {
+//     const { aid } = req.params;
+//     const assignmentIndex = db.assignments.findIndex(
+//     (a) => a._id === aid);
+//     db.assignments[assignmentIndex] = {
+//     ...db.modules[assignmentIndex],
+//     ...req.body
+//     };
+//     res.sendStatus(204);
+// });
 
-}
-export default AssignmentsRoutes;
+// }
+// export default AssignmentsRoutes;
 
