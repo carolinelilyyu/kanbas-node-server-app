8d62717e82b87ad8a8e8e6b52e8a3f146b88cc6f
carolinelilyyu <carolinelilyyu@gmail.com>
2023-12-04 17:38:05 -0500

    a6 and gitignore

--- users/dao.js
+++ users/dao.js
@@ -6,8 +6,6 @@ export const findUserByUsername = (username) =>
   model.findOne({ username: username });
 export const findUserByCredentials = (username, password) =>
   model.findOne({ username, password });
-
 export const updateUser = (userId, user) =>
   model.updateOne({ _id: userId }, { $set: user });
-
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
