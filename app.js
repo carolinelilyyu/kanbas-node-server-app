27fd87691179079714815325f3cafe4e36f0f2c1
carolinelilyyu <carolinelilyyu@gmail.com>
2023-12-07 17:07:57 -0500

    project

--- app.js
+++ app.js
@@ -6,15 +6,29 @@ import Lab5 from "./lab5.js";
 import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
-// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas-cs5610-fa23'
-const CONNECTION_STRING = "mongodb://127.0.0.1:27017/kanbas-cs5610-fa23";
-
-mongoose.connect(CONNECTION_STRING);
-// mongoose.connect("mongodb://127.0.0.1:27017/kanbas-cs5610-fa23");
 import ModuleRoutes from "./modules/routes.js";
 import CourseRoutes from "./courses/routes.js";
-import "dotenv/config";
 // import AssignmentsRoutes from "./assignments/routes.js";
+
+import "dotenv/config";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas-cs5610-fa23'
+// mongoose.connect(CONNECTION_STRING);
+
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://giuseppi:supersecretpassword@cluster0.g1irwku.mongodb.net/kanbas-cs5610-fa23?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Check connection status
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log("Mongo is connected.");
+});
+
 
 const app = express();
 app.use(
